107b922b88548bc89814405b8c05e49fb2b9ee5c
Sommyj1 <joseph2blessing2015@gmail.com>
2023-04-13 19:25:23 -0700

    Task 4: Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,19 +1,30 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if ((w = parseInt(w)) > 0 && (h = parseInt(h)) > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
   }
 
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+
   }
 
   double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+    this.height = this.height * 2;
+    this.width = this.width * 2;
   }
+}
 
-};
+module.exports = Rectangle;
