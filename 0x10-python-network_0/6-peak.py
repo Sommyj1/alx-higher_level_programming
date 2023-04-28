3a7c6213a3c0e91ac3f6d57185f71fe02d511e6f
Sommyj1 <113688978+Sommyj1@users.noreply.github.com>
2023-04-28 15:51:02 -0700

    Delete 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""function"""
-	
-def find_peak(list_of_integers):
-    """pike"""
-    if len(list_of_integers) == 0:
-	return
-    list_of_integers.sort()
-    return list_of_integers[-1]
