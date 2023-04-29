b54d99b362c29fdffab73c477103b65dd20c4f31
Sommyj1 <joseph2blessing2015@gmail.com>
2023-04-29 13:38:48 -0700

    What is my Status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
+        response = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
