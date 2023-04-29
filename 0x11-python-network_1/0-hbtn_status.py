235d6acb029ee05f82d4b002700ce5da5e430d02
Sommyj1 <joseph2blessing2015@gmail.com>
2023-04-29 15:30:39 -0700

    Task 0 What is my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""
-A Python script that fetches https://alx-intranet.hbtn.io/status
-"""
 
-if __name__ == '__main__':
-    import urllib.request
+"""A Script that fetches https://alx-intranet.hbtn.io/status."""
+from urllib.request import Request, urlopen
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
-        response = r.read()
+
+if __name__ == "__main__":
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('utf-8')))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
