4ebf7261865a08a9865609a0363035a36e51cc5d
Sommyj1 <joseph2blessing2015@gmail.com>
2023-04-30 18:22:40 -0700

    Task 4 What is my status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Script that fetches https://alx-intranet.hbtn.io/status."""	
+import requests
+	
+	
+if __name__ == "__main__": 
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type:", type(str(r)))
+    print("\t- content:", r.content.decode("utf-8"))
