83e2071b18bec0e7d6783e4730a5c906a07a204a
Sommyj1 <113688978+Sommyj1@users.noreply.github.com>
2023-04-30 18:57:04 -0700

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,8 +5,9 @@ A script that takes in a URL, sends a request to the URL and displays the value 
 
 import sys
 import requests
+
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = requests.get(url)
-    print(response.headers.get("X-Request-Id"))
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
