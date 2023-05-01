6c5a66f4f95dc2e5d8a97a6ef010e824dbdc548a
Sommyj1 <113688978+Sommyj1@users.noreply.github.com>
2023-04-30 18:49:26 -0700

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Script that fetches https://alx-intranet.hbtn.io/status."""	
 import requests
-	
-	
+		
 if __name__ == "__main__": 
     r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
