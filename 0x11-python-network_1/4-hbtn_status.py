464702f9b0c5ddcc28e8044589be96b031f99843
Sommyj1 <joseph2blessing2015@gmail.com>
2023-04-30 20:15:20 -0700

    task 4 correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Script that fetches https://alx-intranet.hbtn.io/status."""	
+""" A Python script that fetches https://alx-intranet.hbtn.io/status."""
 import requests
-		
-if __name__ == "__main__": 
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type:", type(str(r)))
-    print("\t- content:", r.content.decode("utf-8"))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
